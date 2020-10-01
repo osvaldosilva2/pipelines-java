d83fec27b4635bb04d5dd50732a18e5158395213
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 15:16:41 -0500

    Create UnitTest.java

---
+++ src/main/java/com/microsoft/test/annotations/UnitTest.java
@@ -0,0 +1,18 @@
+package com.microsoft.test.annotations;
+
+public interface UnitTest {
+  /*
+   * This empty interface should be used to categorize
+   * unit tests using the JUnit 4.x @Cateogory annotation.
+   * 
+   * 
+   * A unit test tests a single unit in the system. Ideally 
+   * a unit test should test
+   * one-and-only class, and it should 
+   * not have any external input or output.
+   * Do not use this category if the test 
+   * interacts with a database(not even fixtures), network,
+   * or file system.
+   * 
+   */
+}
