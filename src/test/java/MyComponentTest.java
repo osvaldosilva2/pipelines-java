9a6c4b9fa54b33593aacbd5fdf7db736183cd49c
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 16:03:04 -0500

    Update MyComponentTest.java

--- src/test/java/MyComponentTest.java
+++ src/test/java/MyComponentTest.java
@@ -2,7 +2,7 @@ import com.microsoft.demo.Demo;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Category(com.microsoft.test.annotations.ComponentTests.class)
+@Category(com.microsoft.test.annotations.ComponentTest.class)
 public class MyComponentTest {
     @Test
     public void test_method_1() {
