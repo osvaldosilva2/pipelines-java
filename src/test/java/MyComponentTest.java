63192d6457e337c90d3863261f898881ea83ba69
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 15:56:59 -0500

    Create MyComponentTest.java

---
+++ src/test/java/MyComponentTest.java
@@ -0,0 +1,16 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+import org.junit.experimental.categories.Category
+
+@Category(com.microsoft.test.annotations.ComponentTests.class)
+public class MyComponentTest {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
