a3016a65cd25664a67c4a70d75f6d4c86bfa65fc
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 16:27:22 -0500

    Create Flaky.java

---
+++ src/main/java/com/microsoft/test/annotations/Flaky.java
@@ -0,0 +1,13 @@
+package com.microsoft.test.annotations;
+
+public interface Flaky {
+/*
+ * This empty interface should be used to categorize
+ * tests that fail intermittently using the JUnit 4.x @Cateogory annotation.
+ *  
+ * Use this category to indicate tests that
+ * that fail intermittently especially
+ * when the failures do not indicate a problem
+ * with the code under test. 
+ */
+}
