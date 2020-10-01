75931eaf4a56fe388f71ec8b4bf1830e73cc0799
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 16:02:00 -0500

    Update MyUnitTest.java

--- src/test/java/MyUnitTest.java
+++ src/test/java/MyUnitTest.java
@@ -2,7 +2,7 @@ import com.microsoft.demo.Demo;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Category(com.microsoft.test.annotations.UnitTests.class)
+@Category(com.microsoft.test.annotations.UnitTest.class)
 public class MyUnitTest {
     @Test
     public void test_method_1() {
