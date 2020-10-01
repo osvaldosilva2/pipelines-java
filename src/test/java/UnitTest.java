b03ef6eab2c2d1dbcaf322549be5c86296b1b83f
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 15:01:10 -0500

    Create UnitTest.java

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,14 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+
+public class UnitTest {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
