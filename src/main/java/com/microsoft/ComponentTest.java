3897712e5ecfd7d1656e37f40b6c9d869a96d4fc
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 15:15:03 -0500

    Create ComponentTest.java

---
+++ src/main/java/com/microsoft/ComponentTest.java
@@ -0,0 +1,10 @@
+package com.microsoft;
+
+/**
+ * Used to designate a non-unit test which is not truly a "legacy" test and
+ * should be moved out of this module into a more appropriate structure.
+ * <br>
+ * TODO: Move all tests annotated with this into a more appropriate structure.
+ */
+public interface ComponentTest {
+}
