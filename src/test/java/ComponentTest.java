5fb382b02275a884a4683ebbf5187a009045bfa3
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 14:58:36 -0500

    Update ComponentTest.java

--- src/test/java/ComponentTest.java
+++ src/test/java/ComponentTest.java
@@ -1,1 +1,14 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
 
+public class ComponentTest {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
