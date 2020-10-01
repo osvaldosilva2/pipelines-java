ca3fd036c89739012edaf3e7a6a4487295d1f2e6
osvaldosilva2 <72155076+osvaldosilva2@users.noreply.github.com>
2020-10-01 16:08:53 -0500

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,8 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+@Category(com.microsoft.test.annotations.SmokeTest.class)
 public class MyTest {
     @Test
     public void test_method_1() {
